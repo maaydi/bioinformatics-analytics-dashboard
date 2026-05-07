465c85e0519e245b99acd168ba8d2affda74b43d
Mohamed Ali AIDI <medaliaidi250@gmail.com>
2026-05-07 16:16:13 +0100

    IMPORT-001 migrate DB to support TEXT for protein instead of varchar

---
+++ backend/src/main/resources/db/migration/V3__expand_protein_columns.sql
@@ -0,0 +1,9 @@
+ALTER TABLE protein_entry
+ALTER
+COLUMN protein_ec_number TYPE TEXT,
+    ALTER
+COLUMN entry_name TYPE TEXT,
+    ALTER
+COLUMN gene_name_primary TYPE TEXT,
+    ALTER
+COLUMN protein_short_name TYPE TEXT;
