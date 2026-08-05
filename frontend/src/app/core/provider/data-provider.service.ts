254cb74411d4d130b1b25435d5992f249a5a56b2
Mohamed Ali AIDI <medaliaidi250@gmail.com>
2026-08-05 22:02:12 +0100

    REMOTE-001 Frontend Persist data provider in localStorage

--- frontend/src/app/core/provider/data-provider.service.ts
+++ frontend/src/app/core/provider/data-provider.service.ts
@@ -1,23 +1,42 @@
-import {Injectable, signal} from '@angular/core';
-
+import {effect, inject, Injectable, PLATFORM_ID, signal} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
 export type DataProvider = 'postgres' | 'uniprotKb';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataProviderService {
-  readonly currentProvider = signal<DataProvider>('postgres');
+  private readonly platformId = inject(PLATFORM_ID);
+  private readonly STORAGE_KEY = 'gene-provider';
+
+  #currentProvider = signal<DataProvider>('postgres');
+
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedProvider = localStorage.getItem(this.STORAGE_KEY) as DataProvider;
+      if (savedProvider) {
+        this.setProvider(savedProvider);
+      }
+    }
+
+    effect(() => {
+      const activeProvider = this.#currentProvider();
+      if (isPlatformBrowser(this.platformId)) {
+        localStorage.setItem(this.STORAGE_KEY, activeProvider);
+      }
+    });
+  }
 
   setProvider(provider: DataProvider): void {
-    this.currentProvider.set(provider);
+    this.#currentProvider.set(provider);
   }
 
   toggleProvider(): void {
-    this.currentProvider.update((c) => c === 'postgres' ? 'uniprotKb' : 'postgres');
+    this.#currentProvider.update((c) => (c === 'postgres' ? 'uniprotKb' : 'postgres'));
   }
 
   getProvider(): DataProvider {
-    return this.currentProvider();
+    return this.#currentProvider();
   }
 }
