8c0b14d72b5ce07648231bb5738e3b9fba8ac231
Mohamed Ali AIDI <medaliaidi250@gmail.com>
2026-09-06 11:55:37 +0100

    ARCH-001 PHASE 5 — Extract Export Service (`services/export-service/`) Add Application properties

---
+++ backend/services/export-service/src/main/java/com/bioinformatics/exportservice/config/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package com.bioinformatics.exportservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties(prefix = "app")
+public record ApplicationProperties(@DefaultValue Export export) {
+    public record Export (@DefaultValue Csv csv){
+    }
+
+    public record Csv ( @DefaultValue("100000") int maxRows){
+    }
+}
